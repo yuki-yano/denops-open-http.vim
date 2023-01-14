b45a7c4abb5b08e461a9242d5be62a8bd835aead
yuki_yano <yuki.ycino@gmail.com>
2023-01-14 19:32:43 +0900

    Replace github url to raw

--- denops/open-http/main.ts
+++ denops/open-http/main.ts
@@ -4,17 +4,26 @@ import { define } from "https://deno.land/x/denops_std@v4.0.0/autocmd/mod.ts";
 export const main = async (denops: Denops): Promise<void> => {
   await define(
     denops,
-    "BufEnter",
+    "BufReadCmd",
     "https://*,http://*",
     `call denops#request("${denops.name}", "open_http_file", [])`
   );
 
   denops.dispatcher = {
     open_http_file: async (): Promise<void> => {
-      const url = await denops.call("bufname", "%");
-      const res = await fetch(url as string);
+      let url = (await denops.call("bufname", "%")) as string;
+      const regexp = new RegExp(
+        `github.com/(?<userName>.+)/(?<repoName>.+)/blob/(?<branch>.+)/(?<filePath>.+)`
+      );
+      const result = regexp.exec(url);
+
+      if (result != null) {
+        url = url.replace(`/blob/`, `/raw/`);
+      }
+      const res = await fetch(url);
       const text = await res.text();
       denops.call("append", 0, text.split("\n"));
+      denops.cmd("filetype detect");
     },
   };
 
